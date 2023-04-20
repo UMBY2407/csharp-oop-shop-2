7b7a474dfa0b1ec94938eb64e48bab1a9a67022e
UMBY2407 <umby2407@gmail.com>
2023-04-20 15:22:20 +0200

    Created type of product classes

---
+++ C#Shop2/Negozio.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Shop2
+{
+	public class Negozio
+	{
+		//ATTRIBUTI
+		private string nome;
+		private string citta;
+		private string indirizzo;
+		private int numeroCivico;
+		private List<Prodotto> listaProdotti;
+
+		//COSTRUTTORI
+		public Negozio(string nome, string citta, string indirizzo, int numeroCivico)
+		{
+			this.nome = nome;
+			this.citta = citta;
+			this.indirizzo = indirizzo;
+			this.numeroCivico = numeroCivico;
+			this.listaProdotti = new List<Prodotto>();
+		}
+
+		//GETTERS
+		public string getNome() { return nome; }
+		public string getCitta() { return citta; }
+		public string getIndirizzo() { return indirizzo; }
+		public int getNumeroCivico() { return numeroCivico; }
+		public List<Prodotto> GetListaProdotti()
+		{
+			return this.listaProdotti;
+		}
+
+		//SETTERS
+
+		public void setCitta(string citta)
+		{
+			this.citta = citta;
+		}
+		public void setIndirizzo(string indirizzo)
+		{
+			this.indirizzo = indirizzo;
+		}
+
+		public void setNumeroCivico(int numeroCivico)
+		{
+			this.numeroCivico = numeroCivico;
+		}
+
+		//METODI
+		public void aggiungiProdotto(Prodotto nuovoProdotto)
+		{
+			listaProdotti.Add(nuovoProdotto);
+		}
+
+		public void aggiungiListaProdotti(List<Prodotto> listaProdottiDaAggiungere)
+		{
+			foreach (Prodotto nuovoProdottoDaAggiungere in listaProdotti)
+			{
+				this.listaProdotti.Add(nuovoProdottoDaAggiungere);
+			}
+		}
+
+		public string StampaInfoNegozio()
+		{
+			string rapprStringa = "Nome Negozio: " + this.nome + "\n";
+			rapprStringa += "Città: " + this.citta + "\n";
+			rapprStringa += "Indirizzo: " + this.indirizzo + "\n";
+			rapprStringa += "Numero civico: " + this.numeroCivico + "\n";
+
+			return rapprStringa;
+		}
+
+		public string StampaListaProdotti()
+		{
+			string rapprSringa = "";
+			foreach (Prodotto prodottoScansionato in listaProdotti)
+			{
+				rapprSringa += " - " + prodottoScansionato.GetProdottoStringa() + "\n";
+			}
+			rapprSringa += "\n";
+			return rapprSringa;
+		}
+	}
+}
---
+++ C#Shop2/Prodotto.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Shop2
+{
+	public class Prodotto
+	{
+		//ATTRIBUTI
+		private int codice;
+		private string name;
+		private string description;
+		private string categoria;
+		private float price;
+		private float priceWithIva;
+		private int iva;
+
+		//COSTRUTTORI
+		public Prodotto(string nome, string descrizione, string categoria, float prezzo, int iva)
+		{
+			this.name = nome;
+			this.description = descrizione;
+			this.categoria = categoria;
+			this.price = prezzo;
+			this.iva = iva;
+		}
+
+
+		//GETTER
+		public string getNome()
+		{
+			return this.name;
+		}
+
+		public string getDescription()
+		{
+			return this.description;
+		}
+
+		public string getCategoria()
+		{
+			return this.categoria;
+		}
+
+		public float getPrice()
+		{
+			return this.price;
+		}
+
+		public float getPriceWithIva()
+		{
+			return this.priceWithIva;
+		}
+
+
+		//SETTER
+		public void setNome(string nome)
+		{
+			this.name = nome;
+		}
+
+		public void setDescription(string descrizione)
+		{
+			this.description = descrizione;
+		}
+
+		public void setCategoria(string categoria)
+		{
+			this.categoria = categoria;
+		}
+
+		public void setPrezzo(float prezzo)
+		{
+			this.price = prezzo;
+		}
+
+		public void setIva(int iva)
+		{
+
+			this.iva = iva;
+		}
+
+		public void setPriceWithIva()
+		{
+			this.priceWithIva = price + (price / 100) * iva; ;
+		}
+
+
+		//METODI	
+		private int creaCodice()
+		{
+			Random rdn = new Random();
+			codice = rdn.Next();
+			return this.codice;
+		}
+
+		public void printProduct()
+		{
+			setPriceWithIva();
+			Console.WriteLine("-------------- PRODOTTO -----------------");
+			Console.WriteLine("Codice prodotto: " + creaCodice());
+			Console.WriteLine("Nome prodotto: " + getNome());
+			Console.WriteLine("Descrizione prodotto: " + getDescription());
+			Console.WriteLine("Categoria prodotto: " + getCategoria());
+			Console.WriteLine("Prezzo prodotto (iva esclusa): " + getPrice() + " EUR");
+			Console.WriteLine("Prezzo prodotto (+ iva): " + getPriceWithIva() + " EUR");
+			Console.WriteLine("-------------------------------------------");
+		}
+
+		public string GetProdottoStringa()
+		{
+			setPriceWithIva();
+			string rapprStringa = "Codice prodotto: " + creaCodice() + ", ";
+			rapprStringa += "Nome prodotto: " + this.name + ", ";
+			rapprStringa += "Descrizione del prodotto: " + this.description + ", ";
+			rapprStringa += "Categoria: " + this.categoria + ", ";
+			rapprStringa += "Prezzo prodotto (iva inclusa): " + this.priceWithIva + " EUR";
+
+			return rapprStringa;
+		}
+	}
+}
