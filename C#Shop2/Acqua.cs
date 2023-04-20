575fda773ab735509fcddf22ee9fcdebc833db69
UMBY2407 <umby2407@gmail.com>
2023-04-20 16:57:17 +0200

    Added Bevi(), Riempi(), Svuota() in Acqua class

--- C#Shop2/Acqua.cs
+++ C#Shop2/Acqua.cs
@@ -11,25 +11,74 @@ namespace C_Shop2
 	{
 		//ATTRIBUTI
 		private string marca;
-		private int litri;
+		private double litriBottiglia;
+		private double MaxLitri = 1.5f;
+		private int ph;
+		private string sorgente;
 		private string tipo;
 
 		//COSTRUTTORE
-		public Acqua(string marca, string tipo, int litri, string name, string description, float prezzo, int iva) : base (name, description, prezzo, iva)
+		public Acqua(string marca, string tipo, double litriBottiglia, int ph,string sorgente, string name, string description, float prezzo, int iva) : base (name, description, prezzo, iva)
 		{
 			this.marca = marca;
 			this.tipo = tipo;
-			this.litri = litri;
+			this.litriBottiglia = litriBottiglia;
+			this.ph = ph;
+			this.sorgente = sorgente;
 		}
 
 		//GETTERS
+		public string GetMarca()
+		{
+			return this.marca;
+		}
 
+		public double GetLitri()
+		{
+			return this.litriBottiglia;
+		}
+
+		public string GetTipo()
+		{
+			return this.tipo;
+		}
+
+		public int GetPh()
+		{
+			return this.ph;
+		}
+
+		public string GetSorgente()
+		{
+			return this.sorgente;
+		}
 
 		//SETTERS
 
 
 		//METODI
 
+		public void Bevi(double litriDaBere)
+		{
+			Console.WriteLine("Sto Bevendo: " + litriBottiglia + " litri");
+		}
 
+		public void Riempi(double litri)
+		{
+			if(litriBottiglia + litri <= MaxLitri)
+			{
+				this.litriBottiglia += litri;
+			}
+			else
+			{
+				Console.WriteLine("Mi spiace hai raggiunto la capienza massima");
+				this.litriBottiglia = MaxLitri;
+			}
+		}
+
+		public void Svuota()
+		{
+			this.litriBottiglia = 0f;
+		}
 	}
 }
